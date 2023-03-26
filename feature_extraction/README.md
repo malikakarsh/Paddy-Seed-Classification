2914dc65b34d85db359a117f22213642066facf9
Akarsh Malik <malikakarsh@gmail.com>
2023-03-26 11:06:21 +0530

    [Add] script for determining aspect ratio of paddy seeds

---
+++ feature_extraction/README.md
@@ -0,0 +1,16 @@
+# Feature Extraction
+
+## Objective
+
+To draw proper contour of the paddy seed to extract features such as aspect ratio, angle etc.
+
+## Results
+
+ <img src="img/1.png" alt="alt text" height="700">
+
+## Challenges
+
+Drawing proper contours for images of different brighntess, contrast etc.
+
+<img src="img/2.png" alt="alt text" height="700">
+<img src="img/3.png" alt="alt text" height="700">
---
+++ feature_extraction/script.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import math
+from collections import defaultdict
+
+image = cv2.imread("paddy.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 9, 25)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+
+dilate = cv2.dilate(edged, kernel, iterations=1)
+
+# find contours in the dilated image
+contours, _ = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+
+def get_contour_areas(contours):
+
+    all_areas = []
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        all_areas.append(area)
+
+    return all_areas
+
+
+sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+
+largest_item = sorted_contours[0]
+x, y, w, h = cv2.boundingRect(largest_item)
+aspect_ratio = float(w)/h
+
+image_copy = image.copy()
+
+# contours on a copy of the original image
+cv2.drawContours(image_copy, largest_item, -1, (0, 255, 0), 7)
+# cv2.rectangle(image_copy, (x, y), (x+w, y+h), (255, 0, 0), 2)
+text = f"aspect_ratio={aspect_ratio}"
+cv2.putText(image_copy, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                0.9, (0, 255, 0), 2)
+
+cv2.imshow("contours", image_copy)
+cv2.waitKey(0)
